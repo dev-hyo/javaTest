f7a60c83d28485b351903ec1e88dab5323d7973b
dev-hyo <devcomhyo@gmail.com>
2021-01-06 22:19:46 +0900

    자바연습

---
+++ Chapter3_3.java
@@ -0,0 +1,16 @@
+package javaTest;
+
+//두 개의 주사위의 합이 6이 되는 경우는 몇 가지있을까?
+//합이 6이 되는 경우의 수를 출력하는 프로그램을 작성하자.
+
+public class Chapter3_3 {
+    public static void main(String args[]){
+        for(int i=1; i<=6; i++){
+            for(int j=1; j<=6; j++){
+                if(i+j ==6){
+                    System.out.print("("+i+","+j+")");
+                }
+            }
+        }
+    }
+}
---
+++ Chapter3_6.java
@@ -0,0 +1,24 @@
+package javaTest;
+
+//2와 100사이에 있는 모든 소수를 찾는 프로그램을 작성하라.
+//주어진 정수k를 2부터 k-1까지의 숫자로 나누어서 나머지가 0인 것이 하나라도 있으면 소수가 아니다
+
+public class Chapter3_6 {
+    public static void main(String args[]) {
+        boolean isPrime = false;
+
+        for(int x = 2; x <= 100; x++) {
+            for(int y = 2; y < x; y++) {
+                if(x%y == 0) { // 소수가 아닐 경우
+                    isPrime = true;
+                    break;
+                }
+            }
+
+            if(isPrime == false) {
+                System.out.print(x + " ");
+            }
+            isPrime = false;
+        }
+    }
+}
