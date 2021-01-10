0f277e7c01d233d6bd683bf59661a6aaa0d84caa
dev-hyo <devcomhyo@gmail.com>
2021-01-10 22:34:18 +0900

    자바연습

---
+++ Chapter5/Chapter5_1.java
@@ -0,0 +1,23 @@
+package javaTest.Chapter5;
+
+//MyMetric 이라는 클래스를 작성하고 여기에 킬로미터를 마일로 변환하는 정적 메소드인 KiloToMile()을 작성하라
+//반대로 마일을 킬로미터로 변환하는 정적 메소드 mileToKilo()로 작성하라
+//MyMetricTest 클래스에서 이들 정적 메소드를 호출하여 테스트하자
+
+class MyMetric{
+    private final static double kilo = 1.609;
+
+    static void KiloToMile(double input){
+        System.out.println(input+"을 마일로 바꾸면 "+input/kilo);
+    }
+    static void mileToKilo(double input){
+        System.out.println(input+"을 km로 바꾸면 "+input*kilo);
+    }
+}
+
+public class Chapter5_1 {
+    public static void main(String aggs[]){
+        MyMetric.KiloToMile(1);
+        MyMetric.mileToKilo(1);
+    }
+}
---
+++ Chapter5/Chapter5_2.java
@@ -0,0 +1,35 @@
+package javaTest.Chapter5;
+
+import java.util.Scanner;
+
+public class Chapter5_2 {
+    static int seat[] = new int[10];
+    static int reserved;
+
+    public static void main(String args[]){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("--------------------");
+        for(int i=0; i<10; i++){
+            System.out.print(i+1+" ");
+        }
+        System.out.println();
+        System.out.println("--------------------");
+
+        for(int i:seat){
+            System.out.print(i +" ");
+        }
+        System.out.println();
+        System.out.println("--------------------");
+
+        System.out.print("몇 번째 좌석을 예약하시겠습니까?");
+        reserved = scanner.nextInt();
+
+        seat[reserved-1]=1;
+        System.out.println("예약되었습니다.");
+
+        for(int i:seat){
+            System.out.print(i +" ");
+        }
+    }
+}
