0c72aae004c760b57b35556952f87a505cdab079
dev-hyo <devcomhyo@gmail.com>
2021-01-21 11:36:32 +0900

    2739 구구단

---
+++ BaekJoon/stepThree/gugudan.java
@@ -0,0 +1,14 @@
+package javaTest.BaekJoon.stepThree;
+
+import java.util.Scanner;
+
+public class gugudan {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        for(int i=1; i<=9; i++){
+            System.out.println(N + "*" + i + "=" + (N*i));
+        }
+    }
+}
