f26b1db31337189ec0a9fc96a87c5da4bbab41dc
dev-hyo <devcomhyo@gmail.com>
2021-02-01 19:51:17 +0900

    1193 분수찾기

---
+++ BaekJoon/step8/FindFraction.java
@@ -0,0 +1,15 @@
+package javaTest.BaekJoon.step8;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class FindFraction {
+    public static void main(String args[]) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int fraction = Integer.parseInt(bufferedReader.readLine());
+
+
+    }
+}
